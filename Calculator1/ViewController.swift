41abd84b41249c292909df0e7a9b49056492524f
Morgan Simmons <morgansimmons2525@gmail.com>
2020-07-17 13:37:30 -0500

    Initial Commit

---
+++ Calculator1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator1
+//
+//  Created by Student on 7/17/20.
+//  Copyright © 2020 Morgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
